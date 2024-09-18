c9942d44dc2a241e0de6a5e23f50d441e6f29c7d
duama20180 <accforwork20180@gmail.com>
2024-09-18 23:25:32 +0300

    added the ID property; implemented Clone() method

--- Sereda.TaskPlanner/Sereda.TaskPlanner.Domain.Models/Enums/WorkItem.cs
+++ Sereda.TaskPlanner/Sereda.TaskPlanner.Domain.Models/Enums/WorkItem.cs
@@ -16,6 +16,30 @@ namespace Sereda.TaskPlanner.Domain.Models.Enums
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
 
+        public Guid id { get; set; } //id
+
+        public WorkItem()
+        {
+            id = Guid.NewGuid(); //unic. id
+        }
+
+        public WorkItem Clone()
+        {
+            return new WorkItem
+            {
+                id = this.id,
+                CreationDate = this.CreationDate,
+                DueDate = this.DueDate,
+                Priority = this.Priority,
+                Complexity = this.Complexity,
+                Title = this.Title,
+                Description = this.Description,
+                IsCompleted = this.IsCompleted,
+
+            };
+
+        }
+
         public override string ToString()
         {
             return $"{Title}: due {DueDate:dd.MM.yyyy}, {Priority.ToString().ToLower()} priority";
